cf8e5e4a664e5459b3aa0ff235429518aab6fc25
Dony TCHICAYA <ddieuval15@gmail.com>
2026-07-25 16:55:41 +0100

    Diagnostic temporaire : liste templates/pages/ pour verifier les fichiers presents (bug 500 sur mot-de-passe-oublie)

--- deploy/patch_and_deploy.py
+++ deploy/patch_and_deploy.py
@@ -44,6 +44,7 @@ def _list_dir(path):
 
 _list_dir(f"/home/{PA_USER}/hellobiz/")
 _list_dir(f"/home/{PA_USER}/hellobiz/templates/")
+_list_dir(f"/home/{PA_USER}/hellobiz/templates/pages/")
 
 def _fail(step, r):
     print(f"::error::[{step}] HTTP {r.status_code} -- {r.text[:500]}")
